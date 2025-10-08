49e7b1557b5e0ed5363a1a4997b04fb15a1062bd
sominyun <somin455@naver.com>
2025-10-08 23:59:26 +0900

    feat:#1 카드사 한글명

--- src/main/kotlin/com/wisecard/scheduler/scheduler/dto/CardCompany.kt
+++ src/main/kotlin/com/wisecard/scheduler/scheduler/dto/CardCompany.kt
@@ -1,5 +1,5 @@
 package com.wisecard.scheduler.scheduler.dto
 
-enum class CardCompany {
-    HANA, SHINHAN, HYUNDAI, KOOKMIN, LOTTE, SAMSUNG
+enum class CardCompany(name: String) {
+    HANA("하나"), SHINHAN("신한"), HYUNDAI("현대"), KOOKMIN("국민"), LOTTE("롯데"), SAMSUNG("삼성")
 }
