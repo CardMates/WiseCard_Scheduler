7ed920640c6a73f040da344a18747367a91f7547
sominyun <somin455@naver.com>
2025-10-10 02:52:56 +0900

    feat:#3 promotion dto

--- src/main/kotlin/com/wisecard/scheduler/scheduler/dto/PromotionInfo.kt
+++ src/main/kotlin/com/wisecard/scheduler/scheduler/dto/PromotionInfo.kt
@@ -2,11 +2,11 @@ package com.wisecard.scheduler.scheduler.dto
 
 import java.time.LocalDate
 
-data class Promotion(
+data class PromotionInfo(
     val cardCompany: CardCompany,
     val description: String,
     val imgUrl: String,
     val url: String,
-    val startDate: LocalDate,
+    val startDate: LocalDate?,
     val endDate: LocalDate,
 )
