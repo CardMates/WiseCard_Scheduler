6634f32112c691d96f98201e04f3f2421ff900ac
sominyun <somin455@naver.com>
2025-10-10 02:54:04 +0900

    feat:#3 프로모션 스케쥴러

--- src/main/kotlin/com/wisecard/scheduler/SchedulerApplication.kt
+++ src/main/kotlin/com/wisecard/scheduler/SchedulerApplication.kt
@@ -2,10 +2,12 @@ package com.wisecard.scheduler
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class SchedulerApplication
 
 fun main(args: Array<String>) {
-	runApplication<SchedulerApplication>(*args)
+    runApplication<SchedulerApplication>(*args)
 }
---
+++ src/main/kotlin/com/wisecard/scheduler/grpc/PromotionServiceImpl.kt
@@ -0,0 +1,30 @@
+package com.wisecard.scheduler.grpc
+
+import Promotion
+import PromotionServiceGrpc
+import com.wisecard.scheduler.scheduler.util.logger
+import io.grpc.ManagedChannelBuilder
+import net.devh.boot.grpc.server.service.GrpcService
+
+@GrpcService
+class PromotionServiceImpl(
+    private val grpcProperties: GrpcProperties
+) : PromotionServiceGrpc.PromotionServiceImplBase() {
+
+    fun sendPromotions(promotionList: Promotion.CardPromotionList) {
+        val channel = ManagedChannelBuilder
+            .forAddress(grpcProperties.host, grpcProperties.port)
+            .usePlaintext()
+            .build()
+        val stub = PromotionServiceGrpc.newBlockingStub(channel)
+
+        try {
+            stub.receivePromotions(promotionList)
+            logger.info("(${grpcProperties.host}:${grpcProperties.port})로 카드 프로모션 데이터 전송 완료")
+        } catch (e: Exception) {
+            logger.error("데이터 전송 중 오류 발생: ${e.message}")
+        } finally {
+            channel.shutdown()
+        }
+    }
+}
